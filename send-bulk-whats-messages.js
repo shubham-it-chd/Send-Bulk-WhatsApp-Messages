22d7ef7300789d5eb4928bf7c80a810c0cdcbf3a
shubham-it-chd <39440975+shubham-it-chd@users.noreply.github.com>
2022-03-08 02:28:02 +0530

    Update send-bulk-whats-messages.js

--- send-bulk-whats-messages.js
+++ send-bulk-whats-messages.js
@@ -1,29 +1,28 @@
-//connect with whats app web, open participant to whom you want to send message.. then just run below script in your console
- count = 1;
- function myFunc()
+count = 1;
+ function sendBulkWhatsAppMessages()
 {
   
     messageBox = document.querySelectorAll("[contenteditable='true']")[1];
   
-    message = `MyMessage`; // Replace My Message with your message use   to add spaces to your message
+    message = `MyMessage`; // Replace MyMessage with your message use to add spaces to your message
   
     counter = 50; // Replace 5 with the number of times you want to send your message
   
     for (i = 0; i < counter; i++) {
 		count = count+1;
-        event = document.createEvent("UIEvents");
+        uiEvent = document.createEvent("UIEvents");
         messageBox.innerHTML = `*${count}.*` + message.replace(/ /gm, ''); // test it
-        event.initUIEvent("input", true, true, window, 1);
-        messageBox.dispatchEvent(event);
+        uiEvent.initUIEvent("input", true, true, window, 1);
+        messageBox.dispatchEvent(uiEvent);
   
         eventFire(document.querySelector('span[data-icon="send"]'), 'click');
     }
 }  
-var eventFire = (MyElement, ElementType) => {
-    var MyEvent = document.createEvent("MouseEvents");
-    MyEvent.initMouseEvent
-     (ElementType, true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    MyElement.dispatchEvent(MyEvent);
+var eventFire = (el, elType) => {
+    var mouseEvent = document.createEvent("MouseEvents");
+    mouseEvent.initMouseEvent
+     (elType, true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    el.dispatchEvent(mouseEvent);
 };
 const interval = setInterval(function() {
    myFunc()
